99b7c89824637593defa070b80a8b4b2de8e19bb
lluja1n <s202371270@kfupm.edu.sa>
2026-09-18 21:59:34 +0300

    feature2: backToTop button

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ themeButton.addEventListener("click", function () {
         themeButton.textContent = "Dark Mode";
     }
 });
+
+const backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
